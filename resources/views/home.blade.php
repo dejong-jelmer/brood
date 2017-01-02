b86a70a27532d02a3212fa67ca95e18f8fe401d7
vagrant <vagrant@homestead>
2017-01-02 21:29:09 +0100

    fixed small leters in css, added delivery day in odersblock

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,8 +19,8 @@
                 @include('partials.menu')
             </div>
         </div>
-        <div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-lg-offset-2">
-            <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
+        <div class="col-xs-offset-3 col-sm-offset-3 col-md-offset-2 col-lg-offset-2">
+            <div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
                 @if ($messages->count())
                     @foreach($messages as $message)
                         <div class="alert alert-{{$message->color ?: 'info'}} alert-dismissible" role="alert">
@@ -29,17 +29,12 @@
                         </div>                
                     @endforeach
                 @endif
-            {{-- </div>
-        </div>
-    <div class="row">
-        <div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-lg-offset-2">
-            <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11"> --}}
-            <!-- Orders block -->
+                <!-- Orders block -->     
                 @include('user.bread.partials.ordersblock')
             </div>
         </div>
     </div>
-    {{-- </div> --}}
+   
 @endif
 
 @stop
--- resources/views/user/bread/partials/ordersblock.blade.php
+++ resources/views/user/bread/partials/ordersblock.blade.php
@@ -15,7 +15,7 @@
             </div>
         @else
             <form action="{{ route('user.bread.removeorder') }}" method="post" role="form" class="form-horizontal" >
-                <h4>Je bestelling(en):</h4>
+                <h4>Je bestelling(en) voor aanstaande {{ Helper::nextDeliveryDay($mostRecentOrder->updated_at) }}:</h4>
                 @foreach ($orders as $order)
                     <div class="col-xs-offset-1 col-md-offset-1">
                         <p><button class="btn btn-danger btn-xs" name="id" value="{{ $order->pivot->id }}" data-toggle="tooltip" title="Bestelling verwijderen?"><span class="glyphicon glyphicon-remove" style="margin-top:3px"></span></button> {{ $order->pivot->amount }} x {{ $order->bread }} - {{ $order->pivot->created_at->diffForHumans() }}</p>
